fc579313f26e89feb6b0b58ea031beb6eaa51808
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2022-01-02 19:04:00 +0530

    Input-Form-HTML_Project

--- Input-Form-HTML_Project/JS-code-all-here/inputform_script.js
+++ Input-Form-HTML_Project/JS-code-all-here/inputform_script.js
@@ -1,16 +1,17 @@
-const text = document.querySelector('#name');
+// Checking if name is valid while Inputing
+
+const text = document.querySelector('#text');
 const textError = document.querySelector('.text-error');
 text.addEventListener('input' , function(){
     let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$')
-    if(!nameRegex.test(text.value)){
-        textError.textContent = "Name Is Invalid";
-    }else {
-        (new Input_Form()).name = text.value;
+    if(nameRegex.test(text.value)){
         textError.textContent = " ";
     }
+    else{
+         textError.textContent = "Name is Valid";
+    }
 });
-
-//Salary
+//Getting Output for Salary
 const salary = document.querySelector('#salary');
 const salaryOuput = document.querySelector('.salary-output');
 salaryOutput.textContent = salary.value;
